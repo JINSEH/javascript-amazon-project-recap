64a600186a1b00f2af2974756da28d689c0fa7c4
Jinseh <e.p.gamerstv@gmail.com>
2024-10-18 23:32:50 +0800

    Everything up till 14k

---
+++ data/cart.js
@@ -0,0 +1,63 @@
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
+  productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity : 2
+},
+{
+  productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity : 1
+}];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+    cart.forEach((cartItem) => {
+      if (cartItem.productId === productId) {
+        matchingItem = cartItem;
+      }
+    })
+
+    if (matchingItem) {
+      matchingItem.quantity ++;
+    }
+
+    else {
+      cart.push({
+        productId,
+        quantity : 1
+      })
+    }
+    calculateCartQuantity();
+    console.log(cart);
+    saveToStorage();
+    }
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  })
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+};
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (!(cartItem.productId === productId)) {
+      newCart.push(cartItem);
+    }
+  }) 
+  cart = newCart;
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+export function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  })
+  document.querySelector('.js-checkout-quantity-link').innerHTML = `${cartQuantity} items`
+}
