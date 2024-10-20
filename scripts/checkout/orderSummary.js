bf87d4f18cbc8aca93cc0045d5640a7fb790c17d
Jinseh <e.p.gamerstv@gmail.com>
2024-10-20 13:16:20 +0800

    save data and generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { cart, removeFromCart, updateCartQuantity, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from "../../data/deliveryoptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryoptions.js";
 
 hello();
 
@@ -17,22 +17,13 @@ updateCartQuantity();
   let cartSummaryHTML = '';
 
   cart.forEach((cartItem) => {
-    let matchingItem;
-    products.forEach((product) => {
-      if (cartItem.productId === product.id) {
-        matchingItem = product;
-      }})
+    const productId = cartItem.productId
+    const matchingItem = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionsId;
     
-    let deliveryOption = '';
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
-
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
+    
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
