ab3affa50ce93c3223e8e7fd6d2a3df4750557b0
JINSEH <e.p.gamerstv@gmail.com>
2024-10-26 22:24:45 +0800

    convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,15 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 // import '../data/car.js';
 
 Promise.all([
+  loadProductsFetch(),
   new Promise((resolve) => {
-    loadProducts(() => {
+    loadProductsFetch(() => {
       resolve('value1');
     });
   }),
